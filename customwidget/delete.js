d4c99bbaaf041e97d76e20015caa16a64e3a9de6
StephanSadou <143444292+StephanSadou@users.noreply.github.com>
2023-08-29 00:06:38 +0400

    Update delete.js

--- customwidget/delete.js
+++ customwidget/delete.js
@@ -1,35 +1,9 @@
 (function () {
     let tmpl = document.createElement('template');
     tmpl.innerHTML = 
-    `
-<style>
-
-  body {
-  background: white;
-  }
-
-.btn {
-  align-items: center;
-  background-size: cover;
-  justify-content: center;
-  background-image: url('https://cdn-icons-png.flaticon.com/512/3439/3439691.png');
-  background-color: grey; /* Blue background */
-  border-radius: 5px; /* Remove borders */
-  color: white; /* White text */
-  padding: 12px 15px; /* Some padding */
-  font-size: 16px; /* Set a font size */
-  cursor: pointer; /* Mouse pointer on hover */
-  width: 50px;
-  height: 50px; 
-  background-repeat: no-repeat;
-}
-.btn:hover {
-  background-color: #002354;
-    }
-
-</style>
+`
 <div>
-    <button class="btn"></button>
+    <button class="btn">DELETE</button>
 </div>
 ` ;   
    
