c447fb1bfe202af4d6453a314960a3ab45d6e760
StephanSadou <143444292+StephanSadou@users.noreply.github.com>
2023-09-05 00:21:12 +0400

    Update uploaddocument.js

--- customwidget/uploadfeature/uploaddocument.js
+++ customwidget/uploadfeature/uploaddocument.js
@@ -4,7 +4,6 @@
     `
 <head>
     <link href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css' rel='stylesheet'>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <style>
         * {
             margin: 0;
