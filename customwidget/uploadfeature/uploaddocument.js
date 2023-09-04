aabb5a8bd29cb39c24f246a8514cf59798153c0a
StephanSadou <143444292+StephanSadou@users.noreply.github.com>
2023-09-05 00:20:06 +0400

    Update uploaddocument.js

--- customwidget/uploadfeature/uploaddocument.js
+++ customwidget/uploadfeature/uploaddocument.js
@@ -3,7 +3,6 @@
     tmpl.innerHTML = 
     `
 <head>
-    <link rel="stylesheet" href="uploadstyle.css">
     <link href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css' rel='stylesheet'>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <style>
